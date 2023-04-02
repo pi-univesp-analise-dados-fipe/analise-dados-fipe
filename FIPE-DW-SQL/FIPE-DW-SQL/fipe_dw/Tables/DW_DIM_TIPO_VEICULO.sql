5acdbe5adda48809ddfac0f51b9f210937148862
Jucelino Santos <jucelino-ss@hotmail.com>
2023-04-02 19:33:09 -0300

    Ajustes Tipo Veiculo

--- FIPE-DW-SQL/FIPE-DW-SQL/fipe_dw/Tables/DW_DIM_TIPO_VEICULO.sql
+++ FIPE-DW-SQL/FIPE-DW-SQL/fipe_dw/Tables/DW_DIM_TIPO_VEICULO.sql
@@ -7,3 +7,12 @@
     CONSTRAINT [PK_DW_DIM_TIPO_VEICULO] PRIMARY KEY CLUSTERED ([COD_TIPO_VEICULO] ASC)
 );
 
+/*
+-- dados de cadastro
+INSERT [fipe_dw].[DW_DIM_TIPO_VEICULO] 
+	([COD_TIPO_MODELO], [NME_TIPO_MODELO])
+values
+	(1, 'Carro'), 
+	(2, 'Moto'), 
+	(3, 'Caminhão')
+*/
